bb4c01d4e0d0cff6f8be40ebee009d6b0d3c548c
Trace Harris <traceh@whiletrace.com>
2019-09-08 00:57:52 -0700

    initial models completed for 1st version of api will define a create method for dogs next define test database

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -2,3 +2,33 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    image_filename = models.ImageField(upload_to='static/images/dogs')
+    breed = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    size = models.CharField(max_length=1)
+
+
+class UserDog(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+        )
+    dog = models.ForeignKey(
+        'Dog',
+        on_delete=models.CASCADE
+        )
+    status = models.CharField(max_length=1)
+
+
+class UserPref(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+        )
+    age = models.CharField(max_length=1)
+    gender = models.CharField(max_length=1)
+    size = models.CharField(max_length=2)
+
