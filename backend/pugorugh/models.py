abbc39342ce0253d6d15fd9e56f38953fa4f70dc
Trace Harris <traceh@whiletrace.com>
2019-09-13 02:20:17 -0700

    refined models and completed test suite for models.Dog and models.UserDog tests passing next finsish tests for last model, integrate travis.ci

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -1,17 +1,54 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from multiselectfield import MultiSelectField
+
 
 class Dog(models.Model):
+    MALE = 'm'
+    FEMALE = 'f'
+    UNKNOWN = 'u'
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    XLARGE = 'xl'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (UNKNOWN, 'Unknown')
+        ]
+
+    SIZE_CHOICES = [
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (XLARGE, 'Extra Large'),
+        (UNKNOWN, 'Unknown')
+        ]
     name = models.CharField(max_length=50)
     image_filename = models.ImageField(upload_to='static/images/dogs')
     breed = models.CharField(max_length=50)
     age = models.IntegerField()
-    gender = models.CharField(max_length=1)
-    size = models.CharField(max_length=1)
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default=UNKNOWN
+                              )
+    size = models.CharField(
+        max_length=1,
+        choices=SIZE_CHOICES,
+        default=UNKNOWN
+        )
 
 
 class UserDog(models.Model):
+    LIKED = 'l'
+    DISLIKED = 'd'
+    DOG_STATUS = [
+        (LIKED, 'Liked'),
+        (DISLIKED, 'Disliked')
+        ]
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE
@@ -20,15 +57,61 @@ class UserDog(models.Model):
         'Dog',
         on_delete=models.CASCADE
         )
-    status = models.CharField(max_length=1)
+    status = models.CharField(
+        max_length=1,
+        choices=DOG_STATUS,
+        default=LIKED
+        )
 
 
 class UserPref(models.Model):
+    BABY = 'b'
+    YOUNG = 'y'
+    ADULT = 'a'
+    SENIOR = 's'
+    MALE = 'm'
+    FEMALE = 'f'
+    UNKNOWN = 'u'
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    XLARGE = 'xl'
+
+    AGE_CHOICES = (
+        (BABY, 'Baby'),
+        (YOUNG, 'Young'),
+        (ADULT, 'Adult'),
+        (SENIOR, 'Senior')
+    )
+
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (UNKNOWN, 'Unknown')
+    )
+
+    SIZE_CHOICES = (
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (XLARGE, 'Extra Large'),
+        (UNKNOWN, 'Unknown')
+
+    )
+
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE
         )
-    age = models.CharField(max_length=1)
-    gender = models.CharField(max_length=1)
-    size = models.CharField(max_length=2)
-
+    age = MultiSelectField(
+        choices=AGE_CHOICES,
+        max_choices=4,
+        )
+    gender = MultiSelectField(
+        choices=GENDER_CHOICES,
+        max_choices=4
+        )
+    size = MultiSelectField(
+        choices=SIZE_CHOICES,
+        max_choices=4
+        )
--- backend/pugorugh/tests.py
+++ backend/pugorugh/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
-
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from . import models
 # Create your tests here.
+
+
+class DogTestCase(TestCase):
+    def setUp(self):
+        models.Dog.objects.create(
+            name='George',
+            image_filename='13.jpg',
+            age=12,
+            gender='m',
+            size='xl',
+            )
+
+    def test_dog(self):
+        dog = models.Dog.objects.get(name='George')
+        self.assertIsInstance(dog, models.Dog)
+        self.assertEqual(hasattr(dog, 'age'), True)
+        self.assertEqual(getattr(dog, 'gender'), 'm')
+        self.assertEqual(dog.image_filename, '13.jpg')
+
+
+class UserDogTestCAse(TestCase):
+    def setUp(self):
+
+        self.factory = RequestFactory()
+
+        self.user = User.objects.create(
+            username='testuser',
+            email='loser@loser.com',
+            password='password'
+            )
+
+        models.Dog.objects.create(
+            name='George',
+            image_filename='13.jpg',
+            age=12,
+            gender='m',
+            size='xl',
+            )
+
+    def test_userdog(self):
+        test_dog = models.UserDog.objects.create(
+           dog=models.Dog.objects.get(name='George'),
+           user=self.user
+           )
+        self.assertEqual(test_dog.dog.id, 1)
+        self.assertEqual(test_dog.user.username, 'testuser')
+        self.assertIsInstance(test_dog.dog, models.Dog)
+        self.assertEqual(test_dog.status, 'l')
